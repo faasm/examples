67b55df486721022ea80fbe72617d643c4c0bb49
Carlos Segarra <carlos@carlossegarra.com>
2024-10-18 19:18:45 +0000

    rabe: add native build

--- tasks/rabe.py
+++ tasks/rabe.py
@@ -8,7 +8,7 @@ from tasks.env import EXAMPLES_DIR
 
 
 @task(default=True)
-def build(ctx, clean=False):
+def build(ctx, clean=False, native=False):
     """
     Compile rabe library (in Rust) and C++ bindings into a WASM library
     """
@@ -18,18 +18,29 @@ def build(ctx, clean=False):
         rmtree(join(rabe_dir, "target"))
 
     # First, cross-compile the rust library to WASM
-    cargo_cmd = "cargo build --release --target=wasm32-wasip1"
+    cargo_cmd = "cargo build --release"
+    if not native:
+        cargo_cmd += "--target=wasm32-wasip1"
+
     run(cargo_cmd, shell=True, check=True, cwd=rabe_dir)
 
-    # Install it in the WASM sysroot
-    build_env = get_faasm_build_env_dict()
-    src_lib = join(rabe_dir, "target", "wasm32-wasip1", "release", "librabe.a")
-    dst_lib = join(build_env["FAASM_WASM_LIB_INSTALL_DIR"], "librabe.a")
-    copy(src_lib, dst_lib)
+    if not native:
+        # Install it in the WASM sysroot
+        build_env = get_faasm_build_env_dict()
+
+        src_lib = join(rabe_dir, "target", "wasm32-wasip1", "release", "librabe.a")
+        dst_lib = join(build_env["FAASM_WASM_LIB_INSTALL_DIR"], "librabe.a")
+
+        # TODO: move down
+        copy(src_lib, dst_lib)
 
     # Build the CPP bindings library, and cross-compile it to WASM
     rabe_cpp_dir = join(rabe_dir, "cpp-bindings")
-    build_dir = join(rabe_cpp_dir, "build")
+
+    if native:
+        build_dir = join(rabe_cpp_dir, "build-native")
+    else:
+        build_dir = join(rabe_cpp_dir, "build-wasm")
 
     if clean and exists(build_dir):
         rmtree(build_dir)
@@ -52,19 +63,22 @@ def build(ctx, clean=False):
     run(cmake_cmd, shell=True, check=True, cwd=build_dir, env=work_env)
     run("ninja", shell=True, check=True, cwd=build_dir)
 
-    # Install the library in the WASM sysroot
-    src_lib = join(build_dir, "librabe-cpp.a")
-    dst_lib = join(build_env["FAASM_WASM_LIB_INSTALL_DIR"], "librabe-cpp.a")
-    copy(src_lib, dst_lib)
+    if not native:
+        build_env = get_faasm_build_env_dict()
 
-    # Install the header in the WASM sysroot too
-    src_header = join(rabe_cpp_dir, "rabe_bindings.hpp")
-    dst_header = join(
-        build_env["FAASM_WASM_HEADER_INSTALL_DIR"], "tless_abe.h"
-    )
-    copy(src_header, dst_header)
-    src_header = join(rabe_cpp_dir, "tless_aes.h")
-    dst_header = join(
-        build_env["FAASM_WASM_HEADER_INSTALL_DIR"], "tless_aes.h"
-    )
-    copy(src_header, dst_header)
+        # Install the library in the WASM sysroot
+        src_lib = join(build_dir, "librabe-cpp.a")
+        dst_lib = join(build_env["FAASM_WASM_LIB_INSTALL_DIR"], "librabe-cpp.a")
+        copy(src_lib, dst_lib)
+
+        # Install the header in the WASM sysroot too
+        src_header = join(rabe_cpp_dir, "rabe_bindings.hpp")
+        dst_header = join(
+            build_env["FAASM_WASM_HEADER_INSTALL_DIR"], "tless_abe.h"
+        )
+        copy(src_header, dst_header)
+        src_header = join(rabe_cpp_dir, "tless_aes.h")
+        dst_header = join(
+            build_env["FAASM_WASM_HEADER_INSTALL_DIR"], "tless_aes.h"
+        )
+        copy(src_header, dst_header)
