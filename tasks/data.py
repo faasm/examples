9797ec398f242cb7b37def798748f392cb170998
Carlos <carlos@carlossegarra.com>
2024-10-29 18:13:02 +0000

    crypto: add tless-jwt and rabe-aes (#47)
    * rabe: add aes
    
    * examples: track changes
    
    * opencv: add annotation
    
    * rabe: add native build
    
    * rabe: fix native install
    
    * rabe: fix native build
    
    * rabe: more wip
    
    * rabe wip
    
    * rabe: fix size calculation
    
    * rabe: more
    
    * rabe: add space in wasm build
    
    * rabe: more
    
    * rabe: merge fixes
    
    * rabe: load cpabe
    
    * rabe: fix typo
    
    * rabe: fix
    
    * more cpabe
    
    * wip
    
    * wip
    
    * abe cleanup
    
    * rabe: add sha256
    
    * add cpp-binding
    
    * more abe
    
    * wip
    
    * bar
    
    * f
    
    * rabe: merge to faasm
    
    * cpp: bump dep
    
    * nits: run clang-format
    
    * rabe: fix function compilation
    
    * gha: also print upload logs in case of failure
    
    * gha: fixes
    
    * gha: bump faasm version
    
    * gha; bump faasm
    
    * examples: add data files
    
    * gha: more debug
    
    * gha: more debug
    
    * gha: add note about faasm known issue
    
    * gha: remove unneded debug
    
    * gha: skip jwt test

--- tasks/data.py
+++ tasks/data.py
@@ -1,20 +1,15 @@
+from faasmctl.util.upload import upload_file
 from invoke import task
-from os import makedirs
-from os.path import dirname, exists
-from shutil import copyfile
 from tasks.env import EXAMPLES_DATA_FILES
 
 
 @task(default=True)
-def prepare(ctx):
+def upload(ctx):
     """
-    Prepare data files to test WASM examples
+    Upload shared files needed to run WASM examples
     """
     for p in EXAMPLES_DATA_FILES:
-        path_src = p[0]
-        path_dst = p[1]
+        host_path = p[0]
+        faasm_path = p[1]
 
-        if not exists(dirname(path_dst)):
-            makedirs(dirname(path_dst))
-
-        copyfile(path_src, path_dst)
+        upload_file(host_path, faasm_path)
